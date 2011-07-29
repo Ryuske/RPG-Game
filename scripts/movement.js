dbe80a6800681de03dcde2c52926ace26655f15f
Kenyon Haliwell <kenyon.jh@gmail.com>
2011-07-29 11:43:56 -0700

    Fixing naming convention

--- scripts/movement.js
+++ scripts/movement.js
@@ -3,7 +3,7 @@
  */
 
 function movement(speed) {
-    this.delta = {x: 0, y: 0};
+    this.beta = {x: 0, y: 0};
     this.alpha = {x: GetPersonX(player.name), y: GetPersonY(player.name)};
     this.moving = false;
     this.facing;
@@ -11,22 +11,22 @@ function movement(speed) {
     solid = false;
 
     if (IsKeyPressed(KEY_UP) && !this.moving) {
-        this.delta.y = speed/GetPersonSpeedY(player.name)*-1;
+        this.beta.y = speed/GetPersonSpeedY(player.name)*-1;
         this.facing = this.direction.north;
         QueuePersonCommand(player.name, this.facing, true);
         this.moving = true;
     } else if (IsKeyPressed(KEY_DOWN) && !this.moving) {
-        this.delta.y = speed/GetPersonSpeedY(player.name);
+        this.beta.y = speed/GetPersonSpeedY(player.name);
         this.facing = this.direction.south;
         QueuePersonCommand(player.name, this.facing, true);
         this.moving = true;
     } else if (IsKeyPressed(KEY_RIGHT) && !this.moving) {
-        this.delta.x = speed/GetPersonSpeedX(player.name);
+        this.beta.x = speed/GetPersonSpeedX(player.name);
         this.facing = this.direction.east;
         QueuePersonCommand(player.name, this.facing, true);
         this.moving = true;
     } else if (IsKeyPressed(KEY_LEFT) && !this.moving) {
-        this.delta.x = speed/GetPersonSpeedX(player.name)*-1;
+        this.beta.x = speed/GetPersonSpeedX(player.name)*-1;
         this.facing = this.direction.west;
         QueuePersonCommand(player.name, this.facing, true);
         this.moving = true;
@@ -34,17 +34,17 @@ function movement(speed) {
 		SetPersonFrame(player.name, 1);
     }
 
-		if (((this.facing == this.direction.north || this.facing == this.direction.east) && IsTriggerAt(this.alpha.x+this.delta.x+16, this.alpha.y+this.delta.y, 0))) {
-		    ExecuteTrigger(this.alpha.x+this.delta.x+16, this.alpha.y+this.delta.y, 0);
-		} else if (this.facing == this.direction.south && IsTriggerAt(this.alpha.x+this.delta.x, this.alpha.y+this.delta.y+16, 0)) {
-			ExecuteTrigger(this.alpha.x+this.delta.x, this.alpha.y+this.delta.y+16, 0);
-		} else if (this.facing == this.direction.west && IsTriggerAt(this.alpha.x+this.delta.x, this.alpha.y+this.delta.y, 0)) {
-			ExecuteTrigger(this.alpha.x+this.delta.x, this.alpha.y+this.delta.y, 0);
+		if (((this.facing == this.direction.north || this.facing == this.direction.east) && IsTriggerAt(this.alpha.x+this.beta.x+16, this.alpha.y+this.beta.y, 0))) {
+		    ExecuteTrigger(this.alpha.x+this.beta.x+16, this.alpha.y+this.beta.y, 0);
+		} else if (this.facing == this.direction.south && IsTriggerAt(this.alpha.x+this.beta.x, this.alpha.y+this.beta.y+16, 0)) {
+			ExecuteTrigger(this.alpha.x+this.beta.x, this.alpha.y+this.beta.y+16, 0);
+		} else if (this.facing == this.direction.west && IsTriggerAt(this.alpha.x+this.beta.x, this.alpha.y+this.beta.y, 0)) {
+			ExecuteTrigger(this.alpha.x+this.beta.x, this.alpha.y+this.beta.y, 0);
 		}
     if (this.moving === true && !solid) {
 		QueuePersonCommand(player.name, COMMAND_ANIMATE, true);
-        SetPersonX(player.name, this.alpha.x+this.delta.x);
-        SetPersonY(player.name, this.alpha.y+this.delta.y);
+        SetPersonX(player.name, this.alpha.x+this.beta.x);
+        SetPersonY(player.name, this.alpha.y+this.beta.y);
         this.moving = false;
     }
 }
